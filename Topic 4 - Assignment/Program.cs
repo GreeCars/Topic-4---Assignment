d7480fa37385c99d56363187028ddee26b9883c1
Carsen Greenwood <362570749@GoTVDSB.ca>
2024-09-17 09:38:43 -0400

    Working

--- Topic 4 - Assignment/Program.cs
+++ Topic 4 - Assignment/Program.cs
@@ -1,2 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// Carsen
+string name;
+int age;
+double pay;
+Console.WriteLine("Hello. What is your name?");
+name = Console.ReadLine();
+Console.WriteLine("Hello " + name ,". How old are you?");
