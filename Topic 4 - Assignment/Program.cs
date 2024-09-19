e7c27e210ad5876b4ba00fc87eb61712009bbaca
Carsen Greenwood <362570749@GoTVDSB.ca>
2024-09-19 09:38:42 -0400

    Working

--- Topic 4 - Assignment/Program.cs
+++ Topic 4 - Assignment/Program.cs
@@ -2,9 +2,9 @@
 // Carsen
 using System.Diagnostics;
 
-string name, firstName, lastName, login;
+string name, firstName, lastName, login, item1, item2;
 int age, grade, studentID, age2;
-double salary, average, number1, number2, number3;
+double salary, average, number1, number2, number3, item1Price, item2Price, totalPrice, discount, tax, subtotal;
 
 //Console.WriteLine("Hello. What is your name?");
 //name = Console.ReadLine();
@@ -37,17 +37,46 @@ Console.WriteLine();
 //Console.WriteLine("Grade: \t\t" + grade);
 Console.WriteLine();
 
-Console.WriteLine("Hello. What is your name?");
-name = Console.ReadLine();
-Console.WriteLine("Hi, " + name + "! How old are you?");
-age = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Did you know that in five years you will be " + (age + 5) + " years old? And five years ago you were " + (age - 5) + " years old.");
+//Console.WriteLine("Hello. What is your name?");
+//name = Console.ReadLine();
+//Console.WriteLine("Hi, " + name + "! How old are you?");
+//age = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("Did you know that in five years you will be " + (age + 5) + " years old? And five years ago you were " + (age - 5) + " years old.");
 Console.WriteLine();
 
-Console.WriteLine("Please enter a number:");
-Double.TryParse(Console.ReadLine(), out number1);
-Console.WriteLine("Please enter another number:");
-Double.TryParse(Console.ReadLine(), out number2);
-Console.WriteLine("Please enter a final number:");
-Double.TryParse(Console.ReadLine(), out number3);
-Console.WriteLine("Your selected");
+//Console.WriteLine("Please enter a number:");
+//Double.TryParse(Console.ReadLine(), out number1);
+//Console.WriteLine("Please enter another number:");
+//Double.TryParse(Console.ReadLine(), out number2);
+//Console.WriteLine("Please enter a final number:");
+//Double.TryParse(Console.ReadLine(), out number3);
+//Console.WriteLine("Your selected numbers, when added together and divided by 2, equal to " + ((number1 + number2 + number3) / 2) + ".");
+Console.WriteLine();
+
+Console.WriteLine("Please state the name of an item.");
+item1 = Console.ReadLine();
+Console.WriteLine("Please state the item's price.");
+Double.TryParse(Console.ReadLine(), out item1Price);
+Console.WriteLine("Please state the name of another item.");
+item2 = Console.ReadLine();
+Console.WriteLine("Please state the item's price.");
+Double.TryParse(Console.ReadLine(), out item2Price);
+totalPrice = (item1Price + item2Price);
+discount = (0.2 * totalPrice);
+subtotal = (totalPrice - discount);
+tax = ((totalPrice - discount) * 0.13);
+Console.WriteLine();
+
+Console.WriteLine("Sales Receipt");
+Console.WriteLine();
+Console.WriteLine("Item 1: " + item1);
+Console.WriteLine("Price: " + item1Price.ToString("C"));
+Console.WriteLine("Item 2: " + item2);
+Console.WriteLine("Price: " + item2Price.ToString("C"));
+Console.WriteLine("=====");
+Console.WriteLine("Total: " + totalPrice.ToString("C"));
+Console.WriteLine("Discount (20%): " + discount.ToString("C"));
+Console.WriteLine("Subtotal: " + subtotal.ToString("C"));
+Console.WriteLine("Tax (13%): " + tax.ToString("C"));
+Console.WriteLine("=====");
+Console.WriteLine("Total Owed: " + (subtotal + tax).ToString("C"));
